5123877839950c382f35c5ba5346680fd5c4c089
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-04 23:01:17 +0800

    [ExerciseOOP][ZULUEJA] feat: added practice07 complete

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,28 @@
 package practice07;
 
 public class Klass {
+    private final int number;
+    private Student student;
+
+
+    public Klass(int number) {
+        this.number = number;
+
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", number);
+    }
+
+    public void assignLeader(Student student){
+        this.student = student;
+    }
+
+    public Student getLeader(){
+        return student;
+    }
 }
