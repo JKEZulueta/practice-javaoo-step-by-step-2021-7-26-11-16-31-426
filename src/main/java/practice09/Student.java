7e04a0b6b61afe63368a70b4e1321b90b898e33a
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-05 00:06:19 +0800

    [ExerciseOOP][ZULUEJA] feat: should_person_have_id_name_and_age

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,28 @@
 package practice09;
 
-public class Student {
+
+public class Student extends Person {
+    private final Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+
+    }
+
+    public Klass getKlass(){
+        return klass;
+    }
+
+    @Override
+    public String introduce(){
+        if(this.equals(klass.getLeader())) {
+            return String.format("%s I am a Student. I am Leader of %s.", super.introduce(), klass.getDisplayName());
+            //My name is Tom. I am 21 years old. I am a Student. I am Leader of Class 2.
+
+        }else{
+            return String.format("%s I am a Student. I am at %s.", super.introduce(), klass.getDisplayName());
+            //My name is Tom. I am 21 years old. I am a Student. I am at Class 2
+        }
+    }
 }
