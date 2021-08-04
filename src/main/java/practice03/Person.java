0dc466de730939967725df449cf487497aa4a947
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-04 19:51:32 +0800

    [ExerciseOOP][ZULUEJA] feat: added test03 complete

--- src/main/java/practice03/Person.java
+++ src/main/java/practice03/Person.java
@@ -1,4 +1,26 @@
 package practice03;
 
 public class Person {
+    private int klass;
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return String.format("My name is %s. I am %d years old.", name, age);
+        ////MessageFormat.format("My name is {0}. I am {1} years old.", name, age);
+    }
+
 }
--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,22 @@
 package practice03;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person {
+    private final int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+
+    }
+
+    int getKlass(){
+        return klass;
+    }
+
+    public String introduce(){
+        return String.format("I am a Student. I am at Class %d.", klass);
+    }
+
 }
