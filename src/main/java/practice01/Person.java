b4678f897777cad08950408b6791af63fa2345da
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-04 17:03:27 +0800

    [ExerciseOOP][ZULUEJA] feat: added practice01 changes

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -17,9 +17,10 @@ public class Person {
     }
 
     public String introduce(){
-        String result = ("My name is Tom. I am 21 years old.");
 
-        return result;
+
+        return String.format("My name is %s. I am %d years old.", name, age);
+
     }
 }
 
