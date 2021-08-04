9d92061f370ba015d199bc065f85a58cb4703fd1
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-04 19:52:22 +0800

    [ExerciseOOP][ZULUEJA] feat: added test04 complete

--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,16 @@
 package practice04;
 
-public class Worker {
+public class Worker extends Person{
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    public String introduce(){
+        String worker  = "job";
+        String classification = "Worker";
+        String result = ("My name is "+ getName() + "." + " I am " + getAge() + " years old." + " " + "I am a "+
+        classification + "." + " I have a " +worker+".");
+
+        return result;
+    }
 }
