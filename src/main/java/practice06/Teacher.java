983dd7f827ddff15bd59502733e443b2baaa31e1
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-04 21:24:23 +0800

    [ExerciseOOP][ZULUEJA] feat: finished practice06

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,43 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age){
+        super(name, age);
+    }
+
+
+    public void setKlass(Klass klass){
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce(){
+        if(klass != null) {
+            return super.introduce() + " I am a Teacher. I teach " + klass.getDisplayName() + ".";
+        }else{
+            return super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduceWith(Student student) {
+        if(klass.getNumber() == student.getKlass().getNumber()){
+           return String.format("%s I am a Teacher. I teach %s.",super.introduce(), student.getName());
+           //My name is Tom. I am 21 years old. I am a Teacher. I teach Jerry.
+        } else {
+            return String.format("%s I am a Teacher. I don't teach %s.", super.introduce(), student.getName());
+            //My name is Tom. I am 21 years old. I am a Teacher. I don't teach Jerry.
+        }
+    }
 }
