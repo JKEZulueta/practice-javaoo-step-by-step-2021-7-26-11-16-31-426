4f22b53d6b0961b7e6673b1478499462cd3a9230
JKEZulueta <jan.kyle.zulueta@oocl.com>
2021-08-04 19:52:36 +0800

    [ExerciseOOP][ZULUEJA] feat: added test05 complete

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,26 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+    public Teacher(String name, int age) {
+        super(name, age);
+
+    }
+
+    public Teacher(String name, int age, int klass){
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){
+        return klass!=0?
+                (super.introduce() + " I am a Teacher. I teach Class " + klass + "."):
+                (super.introduce() + " I am a Teacher. I teach No Class.");
+    }
+
+    int getKlass(){
+        return klass;
+    }
+
+
 }
